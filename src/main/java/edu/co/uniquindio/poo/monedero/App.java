6a9923b4c1e9d46bb493c7a6711067e2add02746
emmanuelmurillo310108 <emmanuel.murillou@uqvirtual.edu.co>
2025-11-04 23:51:53 -0500

    first

---
+++ src/main/java/edu/co/uniquindio/poo/monedero/App.java
@@ -0,0 +1,4 @@
+package edu.co.uniquindio.poo.monedero;
+
+public class App {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module edu.co.uniquindio.poo.monedero {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens edu.co.uniquindio.poo.monedero to javafx.fxml;
+    exports edu.co.uniquindio.poo.monedero;
+}
